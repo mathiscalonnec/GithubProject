c5ec9873e3c64b1c1b46cb5c95803a28ed7b1f2a
Mathis <mathis.calonnec@gmail.com>
2021-05-03 16:51:24 +0200

    fix error

--- src/component/Home.js
+++ src/component/Home.js
@@ -29,7 +29,7 @@ export default class Home extends React.Component {
             <View style={{alignItems:"center",flex:1}}>
               <Text style={{marginTop:10, fontSize:20, color:"#EBE7E6"}}>{item.login}</Text>
             </View>
-            <TouchableOpacity style={{height:50,width:50, justifyContent:"center", alignItems:"center"}} onPress={() => this.props.navigation.navigate("Repository", {data:item})}>
+            <TouchableOpacity style={{height:50,width:50, justifyContent:"center", alignItems:"center"}} onPress={() => this.props.navigation.navigate("User", {data:item})}>
               <Text style={{color:"#F07167"}}>More</Text>
             </TouchableOpacity>
           </View>
