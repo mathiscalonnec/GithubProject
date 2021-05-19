70988d6ce8a8457aa7815427d89601426a38eb5c
Mathis <mathis.calonnec@gmail.com>
2021-05-19 18:46:57 +0200

    fix repository in favorite

--- src/component/User.js
+++ src/component/User.js
@@ -31,7 +31,7 @@ export default class User extends React.Component {
             <View style={{alignItems:"center",flex:1}}>
               <Text style={{marginTop:10, fontSize:20, color:"#EBE7E6"}}>{item.name}</Text>
             </View>
-            <TouchableOpacity style={{height:50,width:50, justifyContent:"center", alignItems:"center"}} onPress={() => this.props.navigation.navigate("Repository", {data:item})}>
+            <TouchableOpacity style={{height:50,width:50, justifyContent:"center", alignItems:"center"}} onPress={() => this.props.navigation.navigate("Repository", {data:item, fav:false})}>
               <Text style={{color:"#F07167"}}>More</Text>
             </TouchableOpacity>
           </View>
